3cfa07c94dac80811d5909d07ff91747aacc430a
max-awesome-2 <gelinama@sheridancollege.ca>
2026-03-10 19:16:58 +0000

    Deploying to gh-pages from @ max-awesome-2/Portfolio-Site@9c23a0a3be24ba159c4bdb7e3afbafe88964e40f 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://max-awesome-2.github.io/al-folio/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://max-awesome-2.github.io/portfolio-site/#contact">contact us</a>.'
               }
             ]
           }
